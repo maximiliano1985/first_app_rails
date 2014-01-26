fba099b012f00054c62f8f87ea5ca677f52d841a
Maximiliano Giorgio <maximiliano_giorgio@yahoo.it>
2014-01-26 21:22:20 +0100

    Solving problems when pushing to keroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-FirstApp::Application.config.secret_key_base = 'dbe5436c8abd85f3971506eb2f79ac7007aee35ef1c4fb431b4adb1172e8c6ac9a74ec007785738314dd6f639037e498ad1f0bb31eb0f132dc9815515c37e29c'
+FirstApp::Application.config.secret_key_base = '6983e25c22a15ecb5496b967820928075adc730834ff8ea12ad5221eefadf0c709a1f4c326848d8ec563882a0150b6977e1e179029118480bf52f48f062623c4'
